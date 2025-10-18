8df6ddfc4d67b37622f505a1a47ff99ee9c5529e
Samir Stefano Suarez Rios <106124637+YetAnotherUser24@users.noreply.github.com>
2025-10-18 15:12:48 -0500

    refactor(ConfigGcp): accept "1,2" tuple format (#25)
    * refactor(ConfigGcp): accept "1,2" tuple format
    
    * refactor(ConfigGcp): replace literal_eval with regex for parsing tuple keys
    
    * refactor(tests): rename and update tests for parsing tuple keys
    
    * fix(ConfigGcp): update docstring

--- awive/tests/test_config.py
+++ awive/tests/test_config.py
@@ -15,17 +15,26 @@ def config_gcp() -> ConfigGcp:
     )
 
 
-def test_convert_str_keys_valid(config_gcp: ConfigGcp) -> None:
-    """Test conversion of valid string keys to tuples."""
-    input_dict = {"(0,1)": 1.0, "(2,3)": 2.0}
-    expected = {(0, 1): 1.0, (2, 3): 2.0}
-    converted = config_gcp.convert_str_keys_to_tuples(input_dict)
-    assert converted == expected
+def test_parse_tuple_keys_valid(config_gcp: ConfigGcp) -> None:
+    """Test parsing of valid string keys to tuples."""
+    input_dict = {
+        "(0,1)": 1.0,
+        "(2,3)": 2.0,
+        "4,5": 3.0,
+        "  (  6 , 7 )  ": 4.0,
+    }
+    expected = {(0, 1): 1.0, (2, 3): 2.0, (4, 5): 3.0, (6, 7): 4.0}
+    parsed = config_gcp.parse_tuple_keys(input_dict)
+    assert parsed == expected
 
 
-def test_convert_str_keys_invalid(config_gcp: ConfigGcp) -> None:
-    """Test error when converting invalid string keys."""
-    input_dict = {"invalid": 1.0}
+def test_parse_tuple_keys_invalid(config_gcp: ConfigGcp) -> None:
+    """Test error when parsing invalid string keys."""
+    input_dict = {"invalid": 1.0, "(1,2,3)": 2.0, "(a,b)": 3.0}
     with pytest.raises(ValueError) as exc:  # noqa: PT011
-        config_gcp.convert_str_keys_to_tuples(input_dict)
-    assert "Key 'invalid' is not a tuple" in str(exc.value)
+        config_gcp.parse_tuple_keys(input_dict)
+    assert (
+        "Key 'invalid' is not a valid tuple" in str(exc.value)
+        or ("Key '(1,2,3)' is not a valid tuple" in str(exc.value))
+        or ("Key '(a,b)' is not a valid tuple" in str(exc.value))
+    )
