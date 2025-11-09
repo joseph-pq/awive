49722ee70e50aa96a895ca4ece76b38d947bb7cc
Samir_Suarez <samirssr24@gmail.com>
2025-11-09 15:02:07 -0500

    fix(formatter): update has calibration check

--- awive/preprocess/correct_image.py
+++ awive/preprocess/correct_image.py
@@ -254,10 +254,8 @@ class Formatter:
         if self.preprocessing.image_correction.apply:
             # check if we have calibration parameters directly
             has_calibration = (
-                self.preprocessing.image_correction.lens_camera_matrix
-                is not None
-                and self.preprocessing.image_correction.lens_dist_coeffs
-                is not None
+                self.preprocessing.image_correction.camera_matrix is not None
+                and self.preprocessing.image_correction.dist_coeffs is not None
             )
             # compute undistortion maps once
             if not self._lens_params and has_calibration:
